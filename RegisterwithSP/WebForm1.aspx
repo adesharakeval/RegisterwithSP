3e4870e562ece1573e7ff89357554fc9fdc0b648
adesharakeval <adesharakeval@gmail.com>
2024-04-21 15:22:36 +0530

    codecommit

---
+++ RegisterwithSP/WebForm1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="RegisterwithSP.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Register With Store Procedure :<br />
+        <br />
+        Name:&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        City :&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        State:&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+&nbsp;<br />
+        Mobile No:&nbsp;
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        Id :
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" DataSourceID="SqlDataSource1" DataTextField="Id" DataValueField="Id">
+        </asp:DropDownList>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT [Id] FROM [Table1]"></asp:SqlDataSource>
+&nbsp;<br />
+        <br />
+        <br />
+        <asp:Button ID="btnInsert" runat="server" OnClick="btnInsert_Click" Text="Insert" />
+&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Update" />
+&nbsp;&nbsp;
+        <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ RegisterwithSP/WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace RegisterwithSP
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("sp_Insert", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            cmd.Parameters.AddWithValue("@Name", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@City", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@State", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@MobileNo", Convert.ToInt32(TextBox4.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("sp_Update", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            con.Open();
+            cmd.Parameters.AddWithValue("@Id",DropDownList1.SelectedItem.Text);
+            cmd.Parameters.AddWithValue("@Name", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@City", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@State", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@MobileNo", Convert.ToInt32(TextBox4.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("sp_Delete", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            con.Open();
+            cmd.Parameters.AddWithValue("@Id", DropDownList1.SelectedItem.Text);
+           
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
